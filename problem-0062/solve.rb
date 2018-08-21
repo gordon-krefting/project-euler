90920e9bc5235fd0ce1f2c4ababedba0f78ddd7e
Gordon Krefting <gordon@krefting.org>
2018-08-21 13:50:15 -0400

    tweaks

--- problem-0062/solve.rb
+++ problem-0062/solve.rb
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------------------------------------------------------
 # The cube, 41063625 (345^3), can be permuted to produce two other cubes: 56623104 (384^3) and 66430125 (405^3).
 # In fact, 41063625 is the smallest cube which has exactly three permutations of its digits which are also cube.
-# 
+#
 # Find the smallest cube for which exactly five permutations of its digits are cube.
 #
 # ---------------------------------------------------------------------------------------------------------------------
@@ -14,13 +14,12 @@ end
 
 if __FILE__ == $0
 
-
   # We'll store each cube in a hash of arrays: keyed by 'minimum permutation' (string of sorted digits). When one of
   # the arrays has 5 elements we're done
 
   hash = Hash.new()
 
-  Incrementor.new(300).each {|n|
+  Incrementor.new(300).each { |n|
     cube = n ** 3
 
     key = make_key(cube)
@@ -36,11 +35,8 @@ if __FILE__ == $0
       exit
     end
 
-
     if cube > 100_000_000_000_000
       break
     end
   }
-
-
 end
