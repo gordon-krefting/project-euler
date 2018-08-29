37d8a0a0814c92ac46f41e3f32ee5f9c96e12132
Gordon Krefting <gordon@krefting.org>
2018-08-29 10:01:33 -0400

    p145

--- problem-0145/go/solve.go
+++ problem-0145/go/solve.go
@@ -1,16 +1,51 @@
+// Brute force turned out to be fast enough...
 package main
 
 import "fmt"
+import "errors"
 
 func main() {
-	fmt.Println("Yo")
-	Reverse(2)
+	for n := 1; n < 1000000000; n++ {
+		rev, err := Reverse(n)
+		if err == nil && AllDigitsOdd(n+rev) {
+			fmt.Println(n)
+		}
+	}
 }
 
-/**
- * Reverses the digits of an integer. Returns am error if the number can't be reversed (ends with a 0 is the only failure
- * case I can think of)
- */
-func Reverse(i int) (int, error) {
-	return 1, nil
+// AllDigitsOdd returns true if every digit of the input is odd.
+func AllDigitsOdd(n int) bool {
+	if n == 0 {
+		return false
+	}
+
+	for n > 0 {
+		rem := n % 10
+		if rem%2 == 0 {
+			return false
+		}
+		n /= 10
+
+	}
+	return true
+}
+
+// Reverse reverses the digits of an integer. Returns am error if the number can't be reversed
+// (ends with a 0 is the only failure case I can think of)
+func Reverse(n int) (int, error) {
+	if n == 0 {
+		return 0, errors.New("0 can't be reversed")
+	}
+	newN := 0
+	for n > 0 {
+		rem := n % 10
+		// A special case here... if the last digit is 0 we fail
+		if newN == 0 && rem == 0 {
+			return 0, errors.New("n ends with a 0")
+		}
+		newN *= 10
+		newN += rem
+		n /= 10
+	}
+	return newN, nil
 }
--- problem-0145/go/solve_test.go
+++ problem-0145/go/solve_test.go
@@ -2,21 +2,48 @@ package main
 
 import "testing"
 
-func TestReverse(t *testing.T) {
+func TestAllDigitsOdd(t *testing.T) {
 	var tests = []struct {
+		input int
+		want  bool
+	}{
+		{1, true},
+		{2, false},
+		{222, false},
+		{232, false},
+		{111111121, false},
+		{135, true},
+	}
+	for _, test := range tests {
+		if got := AllDigitsOdd(test.input); got != test.want {
+			t.Errorf("AllDigitsOdd(%v) = %v", test.input, got)
+		}
+	}
+}
+
+func TestReverse(t *testing.T) {
+	tests := []struct {
 		input int
 		want  int
 	}{
-		{1, 11},
+		{1, 1},
 		{12, 21},
 		{123, 321},
 		{567483, 384765},
 	}
-	Reverse(10)
 	for _, test := range tests {
 		if got, _ := Reverse(test.input); got != test.want {
 			t.Errorf("Reverse(%v) = %v", test.input, got)
 		}
 	}
 
+}
+
+func TestReverseError(t *testing.T) {
+	tests := []int{10, 0, 100}
+	for _, test := range tests {
+		if _, err := Reverse(test); err == nil {
+			t.Errorf("Reverse(%v) did not fail", test)
+		}
+	}
 }
